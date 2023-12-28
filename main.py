626b4e9bb43b81545ed28746425144e379c16f2b
santanubiswas <santanubiswas@yahoo.com>
2023-12-27 23:14:56 -0800

    print dataframe

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import os
 import streamlit as st
+import pandas as pd 
 
 
 def main():
@@ -21,6 +22,17 @@ def main():
 
     csv_file = st.file_uploader("Upload a CSV file", type="csv")
     if csv_file is not None:
+        try:
+            st_df = pd.read_csv(csv_file, delimiter=',')
+            #
+            # reset the file pointer to the begining of the file - for agent code to read it - to avoid 
+            # pandas.errors.EmptyDataError: No columns to parse from file - errror 
+            csv_file.seek(0)
+        except pd.errors.EmptyDataError:
+            # Handle empty file case
+            print("The file is empty.")
+
+        st.dataframe(st_df, use_container_width=True, hide_index=True)
 
         agent = create_csv_agent(
             OpenAI(temperature=0), csv_file, verbose=True)
@@ -29,7 +41,11 @@ def main():
 
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
-                st.write(agent.run(user_question))
+                answer=agent.run(user_question)
+                #st.write(agent.run(user_question))
+                #print(answer)
+                st.write(answer)
+
 
 
 if __name__ == "__main__":
