2f95a437db2eab3823ed36db47075cc221a7c4eb
santanubiswas <santanubiswas@yahoo.com>
2023-12-28 22:44:20 -0800

    added nb, upsell, downsell, churn

--- main.py
+++ main.py
@@ -35,26 +35,55 @@ def main():
         # Pivot the DataFrame
         st_df_pivot = st_df.pivot(index='CUSTOMER_ID', columns='MONTH', values= 'MONTHLY_REVENUE')
 
-        # 
-        row_totals = st_df_pivot.sum(axis=1)
+        df_pivot_nb = st_df.pivot(index='CUSTOMER_ID', columns='MONTH', values= 'NEW_BUSINESS')
+        df_pivot_upsell = st_df.pivot(index='CUSTOMER_ID', columns='MONTH', values= 'UPSELL')
+        df_pivot_downsell = st_df.pivot(index='CUSTOMER_ID', columns='MONTH', values= 'DOWNSELL')
+        df_pivot_churn = st_df.pivot(index='CUSTOMER_ID', columns='MONTH', values= 'CHURN')
 
+        # Row level total rr
+        row_totals = st_df_pivot.sum(axis=1)
         # Add the row totals as the first column
         st_df_pivot.insert(0, 'Customer_Totals', row_totals)
-
         column_totals = st_df_pivot.sum(axis=0)
-
         # Create a new DataFrame with the column totals
         totals_df = pd.DataFrame(column_totals.values.reshape(1, -1), columns=st_df_pivot.columns, index=['Totals'])
-
         # Concatenate the original DataFrame with the totals DataFrame
         st_df_pivoted_with_totals = pd.concat([st_df_pivot, totals_df])
 
+
+
+        #row level total new business 
+        nb_row_totals = df_pivot_nb.sum(axis=1)
+        df_pivot_nb.insert(0, 'Customer_Totals', nb_row_totals)
+        nb_column_totals = df_pivot_nb.sum(axis=0)
+        nb_totals_df = pd.DataFrame(nb_column_totals.values.reshape(1, -1), columns=st_df_pivoted_with_totals.columns, index=['New Business'])        
+        
+        #row level total upsell
+        upsell_row_totals = df_pivot_upsell.sum(axis=1)
+        df_pivot_upsell.insert(0, 'Customer_Totals', upsell_row_totals)
+        upsell_column_totals = df_pivot_upsell.sum(axis=0)
+        upsell_totals_df = pd.DataFrame(upsell_column_totals.values.reshape(1, -1), columns=st_df_pivoted_with_totals.columns, index=['Up sell'])        
+              
+        #row level total downsell
+        downsell_row_totals = df_pivot_downsell.sum(axis=1)
+        df_pivot_downsell.insert(0, 'Customer_Totals', downsell_row_totals)
+        downsell_column_totals = df_pivot_downsell.sum(axis=0)
+        downsell_totals_df = pd.DataFrame(downsell_column_totals.values.reshape(1, -1), columns=st_df_pivoted_with_totals.columns, index=['Down sell'])        
+              
+        #row level total churn
+        churn_row_totals = df_pivot_churn.sum(axis=1)
+        df_pivot_churn.insert(0, 'Customer_Totals', churn_row_totals)
+        churn_column_totals = df_pivot_churn.sum(axis=0)
+        churn_totals_df = pd.DataFrame(churn_column_totals.values.reshape(1, -1), columns=st_df_pivoted_with_totals.columns, index=['Churn'])        
+                        
+        # append new business, upsell, downsell, and churn row 
+        st_df_pivoted_with_totals = pd.concat([st_df_pivoted_with_totals, nb_totals_df, upsell_totals_df, downsell_totals_df, churn_totals_df])
         st_df_pivoted_with_totals = st_df_pivoted_with_totals.round(0)
 
 
 
         # st.dataframe(st_df, use_container_width=True, hide_index=True, height=220)
-        st.dataframe(st_df, hide_index=True, height=220)
+        st.dataframe(st_df, hide_index=True, height=300, use_container_width=True)
         st.dataframe(st_df_pivoted_with_totals, use_container_width=True)
 
 
